9498fd3fd55b469e0f06d78429e4ee1470590255
Anwesh43 <anweshthecool0@gmail.com>
2021-07-09 00:35:33 +0530

    adding Renderer class

--- bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
+++ bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
@@ -191,4 +191,27 @@ class BookmarkDiveView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BookmarkDiveView) {
+
+        private val animator : Animator = Animator(view)
+        private val bookmarkDive = BookmarkDive(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bookmarkDive.draw(canvas, paint)
+            animator.animate {
+                bookmarkDive.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bookmarkDive.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
