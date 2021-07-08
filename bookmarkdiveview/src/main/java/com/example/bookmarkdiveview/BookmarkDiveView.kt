7785ea9a9be094bcfa84e2a2074571b58585215e
Anwesh43 <anweshthecool0@gmail.com>
2021-07-09 00:31:34 +0530

    adding BDNode class

--- bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
+++ bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
@@ -127,4 +127,45 @@ class BookmarkDiveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDNode(var i : Int, val state : State = State()) {
+
+        private var next : BDNode? = null
+        private var prev : BDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdatng(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDNode {
+            var curr : BDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
