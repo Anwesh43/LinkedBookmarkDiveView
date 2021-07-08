a1cc3894452ed0d0d25774dd71768abfa680fd2e
Anwesh43 <anweshthecool0@gmail.com>
2021-07-09 00:22:38 +0530

    adding Canvas extension methods

--- bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
+++ bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
@@ -31,3 +31,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawBookmark(size : Float, paint : Paint) {
+    val e : Float = size / eFactor
+    val path : Path = Path()
+    path.moveTo(0f, size / 2)
+    path.lineTo(-size / 2, size / 2 + e)
+    path.lineTo(-size / 2, -size / 2)
+    path.lineTo(size / 2, -size / 2)
+    path.lineTo(size / 2, size / 2 + e)
+    path.lineTo(0f, size / 2)
+    drawPath(path, paint )
+}
+
+fun Canvas.drawBookmarkDive(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc2)
+    save()
+    translate(0f, (h / 2 + size / 2) * (1 - sc1 - sc3))
+    drawBookmark(size, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBookmarkDive(scale, w, h, paint)
+}
