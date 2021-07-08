7db1a8cc47c6a8d4149357714e1aeea63ff4ccd3
Anwesh43 <anweshthecool0@gmail.com>
2021-07-09 00:12:18 +0530

    addin constants and libraries

--- bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
+++ bookmarkdiveview/src/main/java/com/example/bookmarkdiveview/BookmarkDiveView.kt
@@ -1,2 +1,28 @@
 package com.example.bookmarkdiveview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Path
+
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 4.2f
+val eFactor : Float = 2.1f
+val delay : Long = 20
+val rot : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
